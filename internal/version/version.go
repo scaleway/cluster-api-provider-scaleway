c19c8f9733a3380f17ebb7c9c9a7917d3917f4ba
Tomy Guichard <tguichard@scaleway.com>
2026-02-06 18:37:38 +0100

    Fallback to module version from build info if available (#57)

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,5 +1,36 @@
 package version
 
+import (
+	"runtime/debug"
+	"slices"
+)
+
 // Version is the version of cluster-api-provider-scaleway.
 // This variable should be set during build.
-var Version = "dev"
+var Version = defaultVersion
+
+const (
+	defaultVersion = "dev"
+	modulePath     = "github.com/scaleway/cluster-api-provider-scaleway"
+)
+
+func init() {
+	// Do nothing if Version was already overridden during build time.
+	if Version != defaultVersion {
+		return
+	}
+
+	// Find the module version in build info, in case it's imported by another module.
+	info, ok := debug.ReadBuildInfo()
+	if ok {
+		for _, dep := range info.Deps {
+			if dep.Path == modulePath {
+				if slices.Contains([]string{"(devel)", ""}, dep.Version) {
+					break
+				}
+
+				Version = dep.Version
+			}
+		}
+	}
+}
