8cc61eb321d9a304148b21eb0362a546b298c2b0
Tomy Guichard <tguichard@scaleway.com>
2026-07-28 15:26:28 +0200

    fix imports in main

--- cmd/main.go
+++ cmd/main.go
@@ -18,6 +18,7 @@ import (
 	expclusterv1 "sigs.k8s.io/cluster-api/exp/api/v1beta1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/certwatcher"
+	ctrlcontroller "sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
